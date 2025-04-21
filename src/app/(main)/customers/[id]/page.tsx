8a92d90231eef7281ea6d7267d2dad63096c6276
Wayan Danyael Eco <wayan.eco@98labs.com>
2025-04-21 21:37:52 +0800

    update params error

--- src/app/(main)/customers/[id]/page.tsx
+++ src/app/(main)/customers/[id]/page.tsx
@@ -1,9 +1,14 @@
 import { getCustomer } from "@/app/actions/customer/get_customer";
 import { MainCustomerIdPage } from "./components/main";
 
-export default async function Page({ params }: { params: { id: string } }) {
-  const { id } = await params;
-  const { data } = await getCustomer(id);
+type PageProps = {
+  params: {
+    id: string;
+  };
+};
+
+export default async function Page({ params }: PageProps) {
+  const { data } = await getCustomer(params?.id);
 
   return (
     <MainCustomerIdPage customer_info={JSON.parse(JSON.stringify(data))} />
