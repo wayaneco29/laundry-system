e73acb30f0df3e734f78879f4eedb98c6780bde2
Wayan Danyael Eco <wayan.eco@98labs.com>
2025-08-14 13:51:31 +0800

    update nextconfig

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    // Warning: This will allow production builds to complete even with ESLint errors
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
